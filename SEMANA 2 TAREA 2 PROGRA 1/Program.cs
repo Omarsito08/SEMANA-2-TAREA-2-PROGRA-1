f3fe5f99310c2b51992cde6cfb00137ad5fa0a74
braya <braya@DESKTOP-KDI2862>
2022-02-18 09:57:52 -0600

    Agregar archivos de proyecto.

---
+++ SEMANA 2 TAREA 2 PROGRA 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TAREA_2_PROGRA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // EJERCICIO NUMERO 1 PEDIR LA EDAD ALL USUARIO
+            byte edad, calculo;
+            Console.WriteLine("¿CUAL ES TU EDAD?");
+            edad = byte.Parse(Console.ReadLine());
+
+            calculo = edad;
+
+
+            Console.WriteLine("NO APARENTAS {0}", calculo);
+
+            Console.WriteLine("PRESIONE ENTER PARA CONTINUAR");
+            Console.ReadKey();
+
+
+            //EJERCICIO NUMERO 2 PEDIR DOS NUMEROS Y MULTIPLICARLOS
+            byte n1, n2, mult;
+
+            Console.WriteLine("¿INGRESA   UN   NUMERO?");
+            n1 = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("¿INGRESA   OTRO NUMERO?");
+            n2 = Convert.ToByte(Console.ReadLine());
+
+            mult = (byte)(n1 * n2);
+
+            ushort resultado = mult;
+
+            resultado = Convert.ToUInt16(n1 * n2);
+
+
+            Console.WriteLine("EL RESULTADO ES {0}", resultado);
+            Console.WriteLine("PRESIONE ENTER PARA CONTINUAR");
+            Console.ReadKey();
+
+
+            //EJERCICIO NUMERO 3 SUMA Y RESTA DE 2 NUMEROS GRANDES
+            long num1, num2, suma, resta, producto;
+
+            Console.WriteLine("¿INGRESA   UN   NUMERO GRANDE?");
+            num1 = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿INGRESA   OTRO   NUMERO GRANDE?");
+            num2 = long.Parse(Console.ReadLine());
+
+            suma = (long)(num1 + num2);
+            resta = (long)(num1 - num2);
+            producto = (long)(num1 * num2);
+
+            Console.WriteLine("EL RESULTADO DE LA SUMA ES: {0}", suma);
+            Console.WriteLine("EL RESULTADO DE LA RESTA ES: {0}", resta);
+            Console.WriteLine("EL RESULTADO DE LA MULTIPLICACION ES: {0}", producto);
+        }
+    }
+}
